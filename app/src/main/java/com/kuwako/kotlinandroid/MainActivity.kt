76b592670be664d2d57c8f5baa19cc6fd69c3a92
kuwako <kuwako@leverages.jp>
2016-09-22 21:21:10 +0900

    入力した文字を反映する何かができた。

--- app/src/main/java/com/kuwako/kotlinandroid/MainActivity.kt
+++ app/src/main/java/com/kuwako/kotlinandroid/MainActivity.kt
@@ -1,12 +1,16 @@
 package com.kuwako.kotlinandroid
 
+import android.content.Context
 import android.os.Bundle
 import android.support.design.widget.FloatingActionButton
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.Toolbar
+import android.util.Log
 import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
@@ -19,13 +23,27 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(toolbar)
 
         val fab = findViewById(R.id.fab) as FloatingActionButton
-        fab.setOnClickListener(View.OnClickListener { view -> Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG).setAction("Action", null).show() })
+        fab.setOnClickListener(View.OnClickListener {
+            view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG).setAction("Action", null).show() })
 
         val tvHelloWorld = findViewById(R.id.textViewHello) as TextView
         val changeBtn = findViewById(R.id.changeBtn) as Button
-        changeBtn.setOnClickListener { view -> tvHelloWorld.setText(text) }
         val etName = findViewById(R.id.editName) as EditText
-
+        etName.setOnFocusChangeListener { view, hasFocus ->
+            Log.i("@@@focus", hasFocus.toString())
+            if (hasFocus == false) {
+                Log.i("@@@focus", "false")
+            }
+        }
+        changeBtn.setOnClickListener { view ->
+            if (!etName.text.toString().equals("")) {
+                text = etName.text.toString()
+                etName.editableText.clear()
+            }
+            tvHelloWorld.text = text
+            val imn :InputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            imn.hideSoftInputFromWindow(view.windowToken, InputMethodManager.HIDE_NOT_ALWAYS)
+        }
     }
-
 }
